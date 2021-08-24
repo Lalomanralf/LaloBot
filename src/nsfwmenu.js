2226502890514b52b81666c028dd96e8cd3aad4c
Lalomanralf <89461832+Lalomanralf@users.noreply.github.com>
2021-08-24 09:38:23 -0400

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -11,7 +11,7 @@ _El bot nesecita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
 
 Algunas funciones fueron eliminas por errores en el servidor de la India
 
-ву ѕнαη∂υу`
+ву ℒ𝒶𝓁ℴ`
 
 }
 
