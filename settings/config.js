fd5fa0b6609175216c43ecc4f762b36aa6237f8a
Lalomanralf <89461832+Lalomanralf@users.noreply.github.com>
2021-08-24 09:21:53 -0400

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'ShanBot',
-        ownerName: 'Shan',
+        botName: 'LaloBot',
+        ownerName: 'Lalo',
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
