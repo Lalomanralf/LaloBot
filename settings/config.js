1f56233e1221a032bd9d509aa7b2e5c7c92c411e
Lalomanralf <89461832+Lalomanralf@users.noreply.github.com>
2021-08-31 12:04:44 -0400

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,8 +1,6 @@
 const config = {
         botName: 'LaloBot',
         ownerName: 'Lalo',
-        "zeksKey": "X1MwH9ci0U8YgZHEsvlUYP4XGxA",
-        "apiUrl": "https://api.zeks.me",
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
